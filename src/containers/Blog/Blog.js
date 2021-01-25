8914f0b5ad5f22b10a4b5ca332050a5609008f24
trilokmhl <48346388+Trilokmhl@users.noreply.github.com>
2021-01-25 15:13:21 +0530

    new Commit with css issue

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,63 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from ' ../../axios';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import classes from "./Blog.css";
+import axios from " ../../axios";
 
 class Blog extends Component {
-    state={
-        posts:[],
-        selectedPostId : null,
-        error: null
-    }
-    componentDidMount() {
-        axios.get('/posts')
-        .then(Response=>{
-            const posts= Response.data.slice(0,4);
-            const updatedPosts=posts.map(post=>{
-                return{
-                    ...post,
-                    author:'max'
-                }
-            })
-            this.setState({posts:updatedPosts});
-        }).catch(error=>{
-            this.setState({error:error})
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then((Response) => {
+        const posts = Response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "max",
+          };
         });
-    }
-    postSelectedHandler=(id)=>{
-        this.setState({selectedPostId:id})
-    }
-    // https://jsonplaceholder.typicode.com/pots
-    render () {
-        let posts =<p style={{textAlign:"center"}}> Something went wrong </p>
-        if( !this.state.error){
-             posts =this.state.posts.map(post=>{
-                return <Post 
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={()=>this.postSelectedHandler(post.id)} />
-            })
-        }
-        
+        this.setState({ posts: updatedPosts });
+      })
+      .catch((error) => {
+        this.setState({ error: error });
+      });
+  }
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+  // https://jsonplaceholder.typicode.com/pots
+  render() {
+    let posts = <p style={{ textAlign: "center" }}> Something went wrong </p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
         return (
-            <div>
-                <section className="Posts">
-                    {posts }
-                  
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
         );
+      });
     }
+
+    return (
+      <div className={classes.Blog}>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/"> Home</a>{" "}
+              </li>
+              <li>
+                <a href="/"> New Post</a>{" "}
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section
+            className={classes.ram}>{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
